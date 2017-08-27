4baefa3aee9cd241ae60004f681ca1094967d735
Ahmed Abdul Badie <badie.ahmed@icloud.com>
2017-08-27 03:00:32 +0200

    Implement RobinNotification class and its tests

---
+++ Robin/Classes/Date+Scheduler.swift
@@ -0,0 +1,70 @@
+//
+//  Date+Scheduler.swift
+//  Pods
+//
+//  Created by Ahmed Abdelbadie on 8/26/17.
+//
+//
+
+import Foundation
+
+public extension Date {
+    
+    /// Adds a number of minutes to a date.
+    /// > This method can add and subtract minutes.
+    ///
+    /// - Parameter minutes: The number of minutes to add/subtract.
+    /// - Returns: The date after the minutes addition/subtraction.
+    func next(minutes: Int) -> Date {
+        let calendar = Calendar.current
+        var components = DateComponents()
+        components.minute = minutes
+        return (calendar as NSCalendar).date(byAdding: components, to: self, options: NSCalendar.Options(rawValue: 0))!
+    }
+    
+    /// Adds a number of hours to a date.
+    /// > This method can add and subtract hours.
+    ///
+    /// - Parameter hours: The number of hours to add/subtract.
+    /// - Returns: The date after the hours addition/subtraction.
+    func next(hours: Int) -> Date {
+        return self.next(minutes: hours * 60)
+    }
+    
+    /// Adds a number of days to a date.
+    /// >This method can add and subtract days.
+    ///
+    /// - Parameter days: The number of days to add/subtract.
+    /// - Returns: The date after the days addition/subtraction.
+    func next(days: Int) -> Date {
+        return self.next(minutes: days * 60 * 24)
+    }
+    
+    /// Removes the seconds component from the date.
+    ///
+    /// - Returns: The date after removing the seconds component.
+    func removeSeconds() -> Date {
+        let calendar = Calendar.current
+        let components = (calendar as NSCalendar).components([.year, .month, .day, .hour, .minute], from: self)
+        return calendar.date(from: components)!
+    }
+    
+    /// Creates a date object with the given time and offset. The offset is used to align the time with the GMT.
+    ///
+    /// - Parameters:
+    ///   - time: The required time of the form HHMM.
+    ///   - offset: The offset in minutes.
+    /// - Returns: Date with the specified time and offset.
+    static func date(withTime time: Int, offset: Int) -> Date {
+        let calendar = Calendar.current
+        var components = (calendar as NSCalendar).components([.year, .month, .day, .hour, .minute], from: Date())
+        components.minute = (time % 100) + offset % 60
+        components.hour = (time / 100) + (offset / 60)
+        var date = calendar.date(from: components)!
+        if date < Date() {
+            date = date.next(minutes: 60*24)
+        }
+        
+        return date
+    }
+}
--- Robin/Classes/RobinNotification.swift
+++ Robin/Classes/RobinNotification.swift
@@ -1,13 +1,110 @@
 //
-//  RobinNotification.swift
-//  Pods
+// Copyright (c) 2017 Ahmed Abdelbadie <badie.ahmed@icloud.com>
 //
-//  Created by Ahmed Abdelbadie on 8/26/17.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 //
 
-import Foundation
+/// An enum that represents the repeat interval of the notification.
+///
+/// - none: The notification should never repeat.
+/// - hour: The notification should repeat every hour.
+/// - day: The notification should repeat every day.
+/// - week: The notification should repeat every week.
+/// - month: The notification should repeat every month.
+public enum Repeats {
+    case none
+    case hour
+    case day
+    case week
+    case month
+}
 
 public class RobinNotification {
     
+    /// A string assigned to the notification for later access.
+    fileprivate(set) public var identifier: String!
+    
+    /// The body string of the notification.
+    public var body: String!
+    
+    /// The date in which the notification is set to fire on.
+    public var date: Date!
+    
+    /// A dictionary that holds additional information.
+    private(set) public var userInfo: [AnyHashable : Any]!
+    
+    /// The title string of the notification.
+    public var title: String?                  = nil
+    
+    /// The number the notification should display on the app icon.
+    public var badge: NSNumber?                = nil
+    
+    /// The sound name of the notification.
+    public var sound: String                   = "iOSDefaultSound"
+    
+    /// The repeat interval of the notification.
+    public var repeats: Repeats                = .none
+    
+    /// The status of the notification.
+    internal(set) public var scheduled: Bool   = false
+    
+    /// A key that holds the identifier of the notification for UILocalNotification which is tored in the `userInfo` property.
+    public let identifierKey: String          = "RobinNotificationIdentifierKey"
+    
+    public init(identifier: String = UUID().uuidString, body: String, date: Date = Date().next(hours: 1)) {
+        self.identifier = identifier
+        self.body = body
+        self.date = date
+        self.userInfo = [self.identifierKey : self.identifier]
+    }
+    
+    /// Adds a value to the specified key in the `userInfo` property. Note that the value is not added if the key is equal to the `identifierKey`.
+    ///
+    /// - Parameters:
+    ///   - value: The value to set.
+    ///   - key: The key to set the value of.
+    public func setUserInfo(value: Any, forKey key: AnyHashable) {
+        if let keyString = key as? String {
+            if (keyString == self.identifierKey) {
+                return
+            }
+        }
+        self.userInfo[key] = value;
+    }
+    
+    /// Removes the value of the specified key. Note that the value is not removed if the key is equal to the `identifierKey`.
+    ///
+    /// - Parameter key: The key to remove the value of.
+    public func removeUserInfoValue(forKey key: AnyHashable) {
+        if let keyString = key as? String {
+            if (keyString == self.identifierKey) {
+                return
+            }
+        }
+        self.userInfo.removeValue(forKey: key)
+    }
+}
+
+public func ==(lhs: RobinNotification, rhs: RobinNotification) -> Bool {
+    return lhs.identifier == rhs.identifier
+}
+
+public func <(lhs: RobinNotification, rhs: RobinNotification) -> Bool {
+    return lhs.date.compare(rhs.date) == ComparisonResult.orderedAscending
 }
