7479778047a022db956bf1de4201fecf1d83b3de
Ahmed Abdul Badie <badie.ahmed@icloud.com>
2017-08-30 00:32:01 +0200

    Add license to Date+Scheduler

---
+++ Robin/Classes/SystemNotification.swift
@@ -0,0 +1,9 @@
+//
+//  SystemNotification.swift
+//  Pods
+//
+//  Created by Ahmed Abdelbadie on 8/30/17.
+//
+//
+
+import Foundation
---
+++ Robin/Classes/UILocalNotification+SystemNotification.swift
@@ -0,0 +1,9 @@
+//
+//  UILocalNotification+SystemNotification.swift
+//  Pods
+//
+//  Created by Ahmed Abdelbadie on 8/30/17.
+//
+//
+
+import Foundation
