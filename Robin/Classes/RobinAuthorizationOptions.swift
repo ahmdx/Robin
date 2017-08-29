567699aaceef0d80ad83b0c8f6caca1bd8bdf834
Ahmed Abdul Badie <badie.ahmed@icloud.com>
2017-08-29 22:59:43 +0200

    Finalize RobinAuthorizationOptions

--- Robin/Classes/RobinAuthorizationOptions.swift
+++ Robin/Classes/RobinAuthorizationOptions.swift
@@ -1,9 +1,33 @@
 //
-//  RobinAuthorizationOptions.swift
-//  Pods
+// Copyright (c) 2017 Ahmed Abdelbadie <badie.ahmed@icloud.com>
 //
-//  Created by Ahmed Abdelbadie on 8/27/17.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 //
 
-import Foundation
+public struct RobinAuthorizationOptions: OptionSet, RawRepresentable {
+    public let rawValue: UInt
+    
+    public init(rawValue: UInt) {
+        self.rawValue = rawValue
+    }
+    
+    public static let badge = RobinAuthorizationOptions(rawValue: 1 << 0)
+    public static let sound = RobinAuthorizationOptions(rawValue: 1 << 1)
+    public static let alert = RobinAuthorizationOptions(rawValue: 1 << 2)
+}
