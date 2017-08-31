d6da466c05c825d033d461a2d4abe2dd74b2791a
Ahmed Abdul Badie <badie.ahmed@icloud.com>
2017-08-31 05:00:47 +0200

    Clean general tests file

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -32,6 +32,6 @@ class Tests: XCTestCase {
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        Robin.shared.cancelAll()
+        super.tearDown()
     }
 }
