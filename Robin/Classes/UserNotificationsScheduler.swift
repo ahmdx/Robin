e788558856b7caa11631d9acbe303644e19defcd
Ahmed <dev@ahmd.pro>
2017-10-23 23:58:32 +0200

    Implement UserNotificationsScheduler

--- Robin/Classes/UserNotificationsScheduler.swift
+++ Robin/Classes/UserNotificationsScheduler.swift
@@ -20,10 +20,39 @@
 // THE SOFTWARE.
 //
 
-import Foundation
+import UserNotifications
 
-internal class UserNotificationsScheduler: Scheduler {
+@available(iOS 10.0, *)
+internal class UserNotificationsScheduler: Scheduler {    
     func requestAuthorization(forOptions options: RobinAuthorizationOptions) {
+        let center: UNUserNotificationCenter             = UNUserNotificationCenter.current()
+        let authorizationOptions: UNAuthorizationOptions = UNAuthorizationOptions(rawValue: options.rawValue)
+        
+        center.requestAuthorization(options: authorizationOptions) { (granter, error) in }
+    }
+    
+    private func trigger(forDate date: Date, repeats: Repeats) -> UNCalendarNotificationTrigger {
+        var dateComponents: DateComponents = DateComponents()
+        let shouldRepeat: Bool             = repeats != .none
+        let calendar: Calendar             = Calendar.current
+        
+        switch repeats {
+        case .none:
+            dateComponents                 = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: date)
+        case .month:
+            dateComponents                 = calendar.dateComponents([.day, .hour, .minute], from: date)
+        case .week:
+            dateComponents.weekday         = calendar.component(.weekday, from: date)
+            fallthrough
+        case .day:
+            dateComponents.hour            = calendar.component(.hour, from: date)
+            fallthrough
+        case .hour:
+            dateComponents.minute          = calendar.component(.minute, from: date)
+            dateComponents.second          = 0
+        }
+        
+        return UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: shouldRepeat)
     }
     
     func schedule(notification: RobinNotification) -> RobinNotification? {
@@ -35,7 +64,38 @@ internal class UserNotificationsScheduler: Scheduler {
             return nil
         }
         
-        return nil
+        let content                                = UNMutableNotificationContent()
+        
+        if let title                               = notification.title {
+            content.title                          = title
+        }
+        
+        content.body                               = notification.body
+        
+        var sound: UNNotificationSound             = UNNotificationSound.default()
+        if let name = notification.sound.name {
+            if name != RobinNotification.defaultSoundName {
+                sound                              = UNNotificationSound(named: name)
+            }
+        } else {
+            if let notificationSound = notification.sound.sound as? UNNotificationSound {
+                sound                              = notificationSound
+            }
+        }
+        content.sound                              = sound
+        
+        content.userInfo                           = notification.userInfo
+        
+        content.badge                              = notification.badge
+        
+        let trigger: UNCalendarNotificationTrigger = self.trigger(forDate: notification.date, repeats: notification.repeats)
+        
+        let request: UNNotificationRequest         = UNNotificationRequest(identifier: notification.identifier, content: content, trigger: trigger)
+        let center: UNUserNotificationCenter       = UNUserNotificationCenter.current()
+        center.add(request, withCompletionHandler: nil)
+        notification.scheduled                     = true
+        
+        return notification
     }
     
     func reschedule(notification: RobinNotification) -> RobinNotification? {
@@ -48,25 +108,80 @@ internal class UserNotificationsScheduler: Scheduler {
         if notification.scheduled == false {
             return
         }
+        
+        let center: UNUserNotificationCenter = UNUserNotificationCenter.current()
+        center.removePendingNotificationRequests(withIdentifiers: [notification.identifier])
+        notification.scheduled = false
     }
     
     func cancel(withIdentifier identifier: String) {
+        let center: UNUserNotificationCenter = UNUserNotificationCenter.current()
+        center.removePendingNotificationRequests(withIdentifiers: [identifier])
     }
     
     func cancelAll() {
+        let center = UNUserNotificationCenter.current()
+        center.removeAllPendingNotificationRequests()
         print("All scheduled system notifications have been canceled.")
     }
     
     func notification(withIdentifier identifier: String) -> RobinNotification? {
-        return nil
+        let semaphore                         = DispatchSemaphore(value: 0)
+        var notification: RobinNotification?  = nil
+        
+        let center: UNUserNotificationCenter  = UNUserNotificationCenter.current()
+        center.getPendingNotificationRequests { requests in
+            for request in requests {
+                if request.identifier == identifier {
+                    notification = RobinNotification.notification(withSystemNotification: request)
+                    
+                    semaphore.signal()
+                    
+                    break
+                }
+            }
+            semaphore.signal()
+        }
+        
+        let _ = semaphore.wait(timeout: DispatchTime.distantFuture)
+        
+        return notification
     }
     
     func scheduledCount() -> Int {
-        return 0
+        let semaphore                        = DispatchSemaphore(value: 0)
+        var count: Int                       = 0
+        
+        let center: UNUserNotificationCenter = UNUserNotificationCenter.current()
+        center.getPendingNotificationRequests { requests in
+            count = requests.count
+            semaphore.signal()
+        }
+        
+        let _ = semaphore.wait(timeout: DispatchTime.distantFuture)
+        
+        return count
     }
     
     //    MARK:- Testing
     
     func printScheduled() {
+        if (self.scheduledCount() == 0) {
+            print("There are no scheduled system notifications.")
+            return
+        }
+        
+        let semaphore                        = DispatchSemaphore(value: 0)
+        let center: UNUserNotificationCenter = UNUserNotificationCenter.current()
+        center.getPendingNotificationRequests { requests in
+            for request in requests {
+                let notification: RobinNotification = request.robinNotification()!
+                
+                print(notification)
+            }
+            semaphore.signal()
+        }
+        
+        let _ = semaphore.wait(timeout: DispatchTime.distantFuture)
     }
 }
